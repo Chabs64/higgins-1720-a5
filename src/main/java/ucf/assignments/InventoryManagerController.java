7360d6bcd003f6d418f90fdf0bb006c89a89f253
chad <chadhiggins6@gmail.com>
2021-07-19 14:24:40 -0400

    Initial commit Base

---
+++ src/main/java/ucf/assignments/InventoryManagerController.java
@@ -0,0 +1,60 @@
+package ucf.assignments;
+
+/*
+ *
+ *  *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  *  Copyright 2021 Chad Higgins
+ *
+ */
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InventoryManagerController implements Initializable {
+
+
+    FileChooser fileChooser = new FileChooser();
+
+    public TextField AddValue;
+    public TextField AddSerialNumber;
+    public TextField AddName;
+    public TextField SearchTerm;
+    public TableView InventoryTable;
+    public TableColumn Value;
+    public TableColumn SerialNumber;
+    public TableColumn Name;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        fileChooser.setInitialDirectory(new File("C:\\"));
+    }
+
+    public void SearchBar(KeyEvent keyEvent) {
+    }
+
+    public void ScrollTable(MouseEvent mouseEvent) {
+    }
+
+    public void SaveClicked(ActionEvent actionEvent) {
+    }
+
+    public void LoadClicked(ActionEvent actionEvent) {
+    }
+
+    public void CloseClicked(ActionEvent actionEvent) {
+    }
+
+    public void AddClicked(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/ucf/assignments/TSVSave.java
@@ -0,0 +1,11 @@
+/*
+ *
+ *  *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  *  Copyright 2021 Chad Higgins
+ *
+ */
+
+package ucf.assignments;
+
+public class TSVSave {
+}
