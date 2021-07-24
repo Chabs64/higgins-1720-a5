8f4bb9c1a26b768e78e0c8426ee26f16bb020d8c
chad <chadhiggins6@gmail.com>
2021-07-24 10:56:39 -0400

    Basic HTMl Loading complete

--- src/main/java/ucf/assignments/FileManager.java
+++ src/main/java/ucf/assignments/FileManager.java
@@ -57,6 +57,13 @@ public class FileManager {
         try {
             PrintWriter printWriter = new PrintWriter(filepath);
 
+            printWriter.write("Value");
+            printWriter.write("\t");
+            printWriter.write("Serial Number");
+            printWriter.write("\t");
+            printWriter.write("Name");
+            printWriter.write("\n");
+
             for (int i = 0; i < List.size(); i++) {
                 printWriter.write(List.get(i).getValue());
                 printWriter.write("\t");
@@ -130,22 +137,19 @@ public class FileManager {
     {
         ObservableList<InventoryItem> List = FXCollections.observableArrayList();
 
-        if (ExtensionType(filepath.toString()) == 1)
+        if(filepath != null)
         {
-            List = HTMlLoad(filepath);
+            if (ExtensionType(filepath.toString()) == 1) {
+                List = HTMlLoad(filepath);
+            }
+
+            if (ExtensionType(filepath.toString()) == 2) {
+                List = TSVLoad(filepath);
+            }
+
         }
 
-        if (ExtensionType(filepath.toString()) == 2)
-        {
-            List = TSVLoad(filepath);
-        }
-
-        if (List.size() > 0)
-        {
-            return List;
-        }
-
-        return null;
+        return List;
     }
 
     private ObservableList<InventoryItem> TSVLoad(File filepath) {
@@ -170,6 +174,8 @@ public class FileManager {
                 List.add(new InventoryItem(name, serialNumber, value));
             }
 
+            List.remove(0);
+
             scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -180,6 +186,49 @@ public class FileManager {
 
     private ObservableList<InventoryItem> HTMlLoad(File filepath) {
         ObservableList<InventoryItem> List = FXCollections.observableArrayList();
+
+        String value = null;
+        String name = null;
+        String serialNumber = null;
+        String line;
+
+        try {
+            Scanner scanner = new Scanner(filepath);
+
+            while(scanner.hasNextLine())
+            {
+                line = scanner.nextLine();
+                if(line.contains("<tr>"))
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (scanner.hasNextLine())
+                        {
+                            line = scanner.nextLine();
+                            String[] htmlArray1 = line.split("<td>|<th scope=\"col\">");
+                            String[] htmlArray2 = htmlArray1[1].split("</td>|</th>");
+
+                            if (!htmlArray2[0].contains("</tr>")) {
+                                if (i == 0) {
+                                    value = htmlArray2[0];
+                                } else if (i == 1) {
+                                    serialNumber = htmlArray2[0];
+                                } else if (i == 2) {
+                                    name = htmlArray2[0];
+                                }
+                            }
+                        }
+                    }
+                    List.add(new InventoryItem(name, serialNumber, value));
+                }
+            }
+
+            List.remove(0);
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
         return List;
